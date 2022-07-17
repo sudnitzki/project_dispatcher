7fd231234659d4ab4f24a0a6effc1b2248f59140
sudnitzki <sudnitzki@gmail.com>
2022-07-17 18:47:58 +0300

    edit models

--- mysite/dispatcher/models.py
+++ mysite/dispatcher/models.py
@@ -2,38 +2,34 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
-# Create your models here.
-from django.db import models
-
-class Trailer(models.Model):
-    name = models.CharField('Type', max_length=200, help_text='Enter the Type of the trailer (e.g. Refrigerated Trailers)')
+class Producer(models.Model):
+    name = models.CharField('Brand', max_length=200, help_text='Enter the Name of the Producer (e.g. Lamberet)', null=True, blank=True)
     
     def __str__(self):
         return self.name
 
 
+class Trailer(models.Model):
 
-class Truck(models.Model):
-    
-    plate = models.CharField('Plate number', max_length=6)
-    producer = models.ForeignKey('Producer', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField('Summary', max_length=1000, help_text='Trumpas knygos aprašymas')
-    vin = models.CharField('Vin code', max_length=17, help_text='17 characters <a href="https://www.autocheck.com/vehiclehistory/vin-basics')
-    trailer = models.ManyToManyField(Trailer, help_text='Select the Type of the trailer')
+    type = models.CharField('Type', max_length=200, null=True, blank=True)
+    features = models.ForeignKey('Features', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField('Summary', max_length=1000, help_text='Other information', null=True, blank=True)
+    platenr = models.CharField('Plate number', max_length=6, help_text='Plate number', null=True, blank=True)
+    producer = models.ManyToManyField(Producer, help_text='Enter the Name of the producer')
 
     def __str__(self):
-        return self.plate
+        return self.type
     
     def get_absolute_url(self):
-        """Nurodo konkretaus aprašymo galinį adresą"""
-        return reverse('truck-detail', args=[str(self.id)])
+        
+        return reverse('trailer-detail', args=[str(self.id)])
 
 
-class TruckInstance(models.Model):
+class TrailerInstance(models.Model):
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Dispatchers ID for the Truck')
-    truck = models.ForeignKey('Truck', on_delete=models.SET_NULL, null=True) 
-    expected_return = models.DateField('Will be available', null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Dispatchers ID for the Trailer')
+    trailer = models.ForeignKey('Trailer', on_delete=models.SET_NULL, null=True) 
+    dexpected_return = models.DateField('Will be available', null=True, blank=True)
 
     LOAN_STATUS = (
         ('ser', 'Service'),
@@ -46,29 +42,29 @@ class TruckInstance(models.Model):
         max_length=3,
         choices=LOAN_STATUS,
         blank=True,
-        default='s',
-        help_text='Status',
+        default='res',
+        help_text='Statusas',
     )
 
     class Meta:
-        ordering = ['expected_return']
+        ordering = ['dexpected_return']
 
     def __str__(self):
-        return f'{self.id} ({self.truck.plate})'
+        return f'{self.id} ({self.booktrailer.type})'
 
 
-class Producer(models.Model):
+class Features(models.Model):
     
-    brand = models.CharField('Brand', max_length=100)
-    country = models.CharField('country', max_length=100)
+    volume = models.CharField('Volume m3', max_length=100)
+    axles = models.CharField('Number of axles', max_length=100)
 
     class Meta:
-        ordering = ['country', 'brand']
+        ordering = ['axles', 'volume']
 
     def get_absolute_url(self):
-        """Returns the url to access a particular producer instance."""
-        return reverse('producer-detail', args=[str(self.id)])
+        
+        return reverse('trailer-detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.country} {self.brand}'
+        return f'{self.axles} {self.volume}'
