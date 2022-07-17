acd197c299833f259aac70017b0185979348edd0
sudnitzki <sudnitzki@gmail.com>
2022-07-17 19:00:35 +0300

    super_administrator

--- mysite/dispatcher/admin.py
+++ mysite/dispatcher/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Features, Producer, Trailer, TrailerInstance
 # Register your models here.
+
+admin.site.register(Trailer)
+admin.site.register(Features)
+admin.site.register(Producer)
+admin.site.register(TrailerInstance)
