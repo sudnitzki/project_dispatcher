8dd7f39c970a407edfba2d5cc85d323731d16184
sudnitzki <sudnitzki@gmail.com>
2022-07-27 11:57:02 +0300

    trailerlist

--- mysite/dispatcher/urls.py
+++ mysite/dispatcher/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('features/', views.features, name='features'),
+    path('trailers/', views.TrailerListView.as_view(), name='trailers'),
+
 ]
--- mysite/dispatcher/views.py
+++ mysite/dispatcher/views.py
@@ -1,7 +1,8 @@
+from msilib.schema import Feature
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Trailer, Features, TrailerInstance, Producer
-
+from django.views import generic
 
 
 def index(request):
@@ -11,18 +12,36 @@ def index(request):
     num_instances = TrailerInstance.objects.all().count()
     
     
-    num_instances_available = TrailerInstance.objects.filter(status__exact='g').count()
+    num_instances_available = TrailerInstance.objects.filter(status__exact='rea').count()
     
-    # Kiek yra autorių    
+     
     num_features = Features.objects.count()
     
-    # perduodame informaciją į šabloną žodyno pavidale:
+    
     context = {
-        'num_books': num_trailers,
+        'num_trailers': num_trailers,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_features,
+        'num_features': num_features,
     }
 
     # renderiname index.html, su duomenimis kintamąjame context
     return render(request, 'index.html', context=context)
+
+def features(request):
+    
+    features = Features.objects.all()
+    context = {
+        'features': features
+    }
+    print(features)
+    return render(request, 'features.html', context=context)
+
+
+class TrailerListView(generic.ListView):
+    model = Trailer
+    template_name = 'trailer_list.html'
+
+class TrailerDetailView(generic.DetailView):
+    model = Trailer
+    template_name = 'trailer_detail.html'
